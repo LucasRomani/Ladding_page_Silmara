c099d20a1358e43c8895bfbcf1d12b866183c9d8
LucasRomani <lucasromaniv@hotmail.com>
2025-06-02 10:35:32 -0300

    novo valor do curso

--- src/body/Payment.jsx
+++ src/body/Payment.jsx
@@ -5,7 +5,7 @@ const Payment = () => {
         <>
             <div className="pay">
             <a href="https://pay.kiwify.com.br/xYhllZV" target="_blank" rel="noopener noreferrer" className="payment-button">
-                INSCREVA-SE, POR R$29,82 POR MÊS
+                INSCREVA-SE, POR R$30,72 POR MÊS
                 </a>
                 <p className="data-protection">
                     🔒 Seus dados estão protegidos. Utilizamos criptografia e não compartilhamos suas informações.
