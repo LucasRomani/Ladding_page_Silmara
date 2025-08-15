eab7f2a5bf1d4907e88fce546d55adf3946778a3
LucasRomani <lucasromaniv@hotmail.com>
2025-08-15 10:48:12 -0300

    Atualização: integração do Pixel do Facebook no botão inscreva-se

--- src/body/Payment.jsx
+++ src/body/Payment.jsx
@@ -1,17 +1,29 @@
 import "./Payment.css";
 
 const Payment = () => {
+    const handleLeadClick = () => {
+        if (typeof fbq === "function") {
+            fbq("track", "Lead");
+        }
+        console.log("Evento Lead enviado!");
+    };
+
     return (
-        <>
-            <div className="pay">
-            <a href="https://pay.kiwify.com.br/xYhllZV" target="_blank" rel="noopener noreferrer" className="payment-button">
+        <div className="pay">
+            <a 
+                href="https://pay.kiwify.com.br/xYhllZV" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="payment-button"
+                onClick={handleLeadClick}
+            >
                 INSCREVA-SE, POR R$30,72 POR MÊS
-                </a>
-                <p className="data-protection">
-                    🔒 Seus dados estão protegidos. Utilizamos criptografia e não compartilhamos suas informações.
+            </a>
+            <p className="data-protection">
+                🔒 Seus dados estão protegidos. Utilizamos criptografia e não compartilhamos suas informações.
             </p>
-            </div>
-        </>
-    )
-}
+        </div>
+    );
+};
+
 export default Payment;
